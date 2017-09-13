c6c0c61a208bede05afddea0bbedf13ad95b74d9
exenone <seeunuwan@gmail.com>
2017-09-13 13:22:20 +0200

    /intial commit

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 const express=require('express');
 const app=express();
 var mongoose = require("mongoose");
+var path = require('path');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override')
+var logger = require('express-logger');
+var pug = require('pug');
+//var jadebootstrap=require('jade-bootstrap');
+
+
 mongoose.Promise = global.Promise;mongoose.connect("mongodb://localhost:27017/mydb");
 
 
+var http = require('http');
+var hostname = 'localhost';
+app.set('port', process.env.PORT || 3000);
 
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/src/views');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src/views')));
+app.use(bodyParser.json());
+app.use(logger({path: "/path/to/logfile.txt"}));
 
-
-
-const http = require('http');
-
-const hostname = 'localhost';
-const port = 3000;
-
-app.use(express.static('public'));
-app.use(express.static('src/views'));
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 //-----------------------------------------------------------------------------------------
-app.use("/", (req, res) => {
+/*app.use("/", (req, res) => {
   res.sendFile(__dirname + "./src/views/fixture.html");
- });
+ });*/
 
  var nameSchema = new mongoose.Schema({
   firstName: String,
@@ -28,42 +38,32 @@ app.use("/", (req, res) => {
 
  var User = mongoose.model("User", nameSchema);
 
- app.post("/addname", (req, res) => {
+ app.post("/addname",urlencodedParser,function(req,res){
+   new User({
+    firstName: req.body.firstName,
+    lastNameName: req.body.lastName
+
+   }).save(function(err,doc){
+     if(err)res.json(err)
+      //else res.send('Success!!');
+     else res.redirect('/view');
+   });
   
  });
 
 
- app.post("/addname", (req, res) => {
-  var myData = new User(req.body);
-  console.log(req.body);
-  myData.save()
-  .then(item => {
-  res.send("item saved to database");
-  })
-  .catch(err => {
-  res.status(400).send("unable to save to database");
-  });
- });
+ app.get('/view',urlencodedParser, function(req, res){
+	User.find({}, function(err, docs){
+		if(err) res.json(err);
+  //	else   res.render('results', {users: docs});
+  else   res.render('work', {users: docs});
+	});
+});
 
 
-  // ---------------------------------------
-app.get('/',function(req,res){
-    res.send('welcome!');
-  
-  });
-  
-  app.get('/routing',function(req,res){
-    res.send('welcome!its routing');
-  
-  });
 
-  app.all('/secret', function (req, res, next) {
-    console.log('Accessing the secret section ...')
-    next() 
-  });
+//---------------------------------------------------------------------------------------
 
-  app.listen(port, hostname, (err) => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  });
-
-  
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
